2d5a7e903b2a82769a947d3a356cc50931225b12
unknown <User@.(none)>
2015-05-29 15:21:13 +0300

    sdkjf

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<?php
+include 'head.html';
+?>
+    <title>OOP and DB</title>
+    <h2>OOP</h2>
+    <a href="oop1-1.php"><h3>OOP Task 1. Practice 1: PHP basics</h3></a>
+    <a href="oop1-2.php"><h3>OOP Task 1. Practice 2: PHP basics</h3></a>
+    <a href="oop1-3.php"><h3>OOP Task 1. Practice 3: PHP basics</h3></a>
+    <a href="oop1-4.php"><h3>OOP Task 1. Practice 4: PHP basics</h3></a>
+    <hr>
+    <a href="oop2.php">  <h3>OOP Task 2. Practice 1: arrays</h3></a>
+    <hr>
+    <a href="oop3.php">  <h3>OOP Task 3. Practice 1: strings</h3></a>
+    <hr>
+    <a href="oop4-1.php"><h3>OOP Task 4. Practice 1: classes</h3></a>
+    <a href="oop4-2-1.php"><h3>OOP Task 4. Practice 2.1: classes</h3></a>
+    <a href="oop4-2-2.php"><h3>OOP Task 4. Practice 2.2: classes</h3></a>
+    <a href="oop4-3.php"><h3>OOP Task 4. Practice 3: classes</h3></a>
+    <hr>
+
+    <h2>DB</h2>
+    <h4>Должна существовать база данных shop_kush. <br>
+    Файл обращается к базе по адресу localhost, логин "root", без пароля.<br>
+    Настройки могут быть изменены в файле connect.php
+    </h4>
+    <a href="DB1-1.php"><h3>DB Task 1. Practice 1: basics</h3></a>
+    <a href="DB2-1.php"><h3>DB Task 2. Practice 1: SELECT</h3></a>
+    <a href="DB3-1.php"><h3>DB Task 3. Practice 1: advanced SELECT</h3></a>
+    <a href="DB4-1.php"><h3>DB Task 4. Practice 1: EXISTS, BETWEEN, ORDER BY, ALL</h3></a>
+
+    <hr>
+    <h2>MREO</h2>
+    <a href="MREO"><h3>OOP Task5. Practice 1: Program for MREO</h3></a>
+    <a href="MREO"><h3>OOP Task5. Practice 2: Program for MREO. AJAX</h3></a>
+
+    <hr>
+    <h2>BONES</h2>
+    <a href="bones"><h3>OOP Task6. Practice 1: PHP + MySQL(PDO)</h3></a>
+    <a href="bones"><h3>OOP Task6. Practice 2: PHP + MySQL(PDO)</h3></a>
+
+    <hr>
+    <h2>OOP</h2>
+    <a href="oop7.php"><h3>OOP Task7. Practice 1: PHP miscellaneous</h3></a>
+
+<?php
+include 'foot.html';
+?>
